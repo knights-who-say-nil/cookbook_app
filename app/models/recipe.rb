2b463c609e9fde0f2ccf27ff0f57fca28712f83c
JackMarx <seriousfools@gmail.com>
2019-09-03 12:32:25 -0500

    model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,23 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  def directions_list
+    directions.split(", ")
+  end
+
+  def formatted_prep_time
+    # input: 125
+    # output: "2 Hours, 5 Minutes"
+
+    # 1 - find number of hours in prep_time
+    # 2 - find remainder of minutes in prep_time
+    # 3 - combine hours and minutes in a formatted string
+
+    hours = prep_time / 60
+    minutes = prep_time % 60
+
+    "#{hours} Hours, #{minutes} Minutes"
+  end
 end
