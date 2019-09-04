4b7fdcca680268f09f694e957f755192b2aa8cf4
JackMarx <seriousfools@gmail.com>
2019-09-04 09:55:51 -0500

    model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,17 +7,20 @@ class Recipe < ApplicationRecord
     directions.split(", ")
   end
 
+  def friendly_created_at
+    created_at.strftime("%b %d, %Y")
+  end
+
   def formatted_prep_time
-    # input: 125
-    # output: "2 Hours, 5 Minutes"
-
-    # 1 - find number of hours in prep_time
-    # 2 - find remainder of minutes in prep_time
-    # 3 - combine hours and minutes in a formatted string
-
     hours = prep_time / 60
     minutes = prep_time % 60
+    
+    time_string = ""
 
-    "#{hours} Hours, #{minutes} Minutes"
+    time_string += "#{hours} #{'Hour'.pluralize(hours)}" if hours > 0
+    time_string += ", " if hours > 0 && minutes > 0
+    time_string += "#{minutes} #{'Minute'.pluralize(minutes)}" if minutes > 0
+
+    time_string
   end
 end
