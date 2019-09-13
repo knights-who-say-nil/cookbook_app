5e92c1158def127e66812188df04e525f00f5f01
JackMarx <seriousfools@gmail.com>
2019-09-13 14:17:36 -0500

    update

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,9 +1,5 @@
 class Api::RecipesController < ApplicationController
   def index
-    puts "=" * 50
-    p current_user
-    puts "=" * 50
-
     @recipes = Recipe.all
 
     search_term = params[:search]
