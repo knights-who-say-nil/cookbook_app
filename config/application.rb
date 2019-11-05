be56c54d3dd46a485059fc1ad6a9ce53b5f2cc0e
JackMarx <seriousfools@gmail.com>
2019-11-05 09:51:20 -0600

    precompile line

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module CookbookApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.autoloader = :classic
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
